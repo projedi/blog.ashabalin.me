bf8a2a1a78e5e320502db92dd5927bd26d0d8db0
Alexander Shabalin <shabalyn.a@gmail.com>
2014-12-17 20:06:07 +0300

    Change code style

--- site.hs
+++ site.hs
@@ -1,67 +1,60 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
 
+import Data.Monoid((<>))
 
---------------------------------------------------------------------------------
+import Hakyll
+
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+   match "images/*" $ do
+      route idRoute
+      compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+   match "css/*" $ do
+      route idRoute
+      compile compressCssCompiler
 
-    match (fromList ["about.markdown", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+   match (fromList ["about.markdown", "contact.markdown"]) $ do
+      route $ setExtension "html"
+      compile $   pandocCompiler
+              >>= loadAndApplyTemplate "templates/default.html" defaultContext
+              >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+   match "posts/*" $ do
+      route $ setExtension "html"
+      compile $   pandocCompiler
+              >>= loadAndApplyTemplate "templates/post.html" postContext
+              >>= loadAndApplyTemplate "templates/default.html" postContext
+              >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+   create ["archive.html"] $ do
+      route idRoute
+      compile $ do
+         posts <- recentFirst =<< loadAll "posts/*"
+         makeItem "" >>= loadAndApplyTemplate "templates/archive.html" (archiveContext posts)
+                     >>= loadAndApplyTemplate "templates/default.html" (archiveContext posts)
+                     >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+   match "index.html" $ do
+      route idRoute
+      compile $ do
+         posts <- recentFirst =<< loadAll "posts/*"
+         getResourceBody >>= applyAsTemplate (indexContext posts)
+                         >>= loadAndApplyTemplate "templates/default.html" (indexContext posts)
+                         >>= relativizeUrls
 
+   match "templates/*" $ compile templateCompiler
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+indexContext :: [Item String] -> Context String
+indexContext posts =  listField "posts" postContext (return posts)
+                   <> constField "title" "Home"
+                   <> defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+archiveContext :: [Item String] -> Context String
+archiveContext posts =  listField "posts" postContext (return posts)
+                     <> constField "title" "Archives"
+                     <> defaultContext
 
-    match "templates/*" $ compile templateCompiler
-
-
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postContext :: Context String
+postContext =  dateField "date" "%B %e, %Y"
+            <> defaultContext
