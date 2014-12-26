6bbb7548a891078b718b346a3fe9688e49e08087
Alexander Shabalin <shabalyn.a@gmail.com>
2014-12-26 22:58:39 +0300

    Add ability to include sources into posts from files

--- site.hs
+++ site.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Applicative
+
+import System.FilePath((</>), dropExtension)
 import Data.Monoid((<>))
+
+import Text.Pandoc(Block(..), Pandoc)
+import Text.Pandoc.Walk(walk)
 
 import Hakyll
 
@@ -14,24 +20,15 @@ main = hakyll $ do
       route idRoute
       compile compressCssCompiler
 
-   match "posts/*" $ do
-      route $ setExtension "html"
-      compile $   pandocCompiler
-              >>= loadAndApplyTemplate "templates/post.html" postContext
-              >>= loadAndApplyTemplate "templates/default.html" postContext
-              >>= relativizeUrls
-
-   match "drafts/*" $ do
-      route $ setExtension "html"
-      compile $   pandocCompiler
-              >>= loadAndApplyTemplate "templates/draft.html" draftContext
-              >>= loadAndApplyTemplate "templates/default.html" draftContext
-              >>= relativizeUrls
+   match "posts/**/*" extraPostFilesRules
+   match "posts/*" $ postRules "templates/post.html" postContext
+   match "drafts/**/*" extraPostFilesRules
+   match "drafts/*.markdown" $ postRules "templates/draft.html" draftContext
 
    create ["index.html"] $ do
       route idRoute
       compile $ do
-         posts <- recentFirst =<< loadAll "posts/*"
+         posts <- recentFirst =<< loadAll "posts/*.markdown"
          makeItem "" >>= loadAndApplyTemplate "templates/index.html" (indexContext posts)
                      >>= loadAndApplyTemplate "templates/default.html" (indexContext posts)
                      >>= relativizeUrls
@@ -39,12 +36,48 @@ main = hakyll $ do
    create ["drafts.html"] $ do
       route idRoute
       compile $ do
-         posts <- loadAll "drafts/*"
+         posts <- loadAll "drafts/*.markdown"
          makeItem "" >>= loadAndApplyTemplate "templates/drafts.html" (draftsContext posts)
                      >>= loadAndApplyTemplate "templates/default.html" (draftsContext posts)
                      >>= relativizeUrls
 
    match "templates/*" $ compile templateCompiler
+
+extraPostFilesRules :: Rules ()
+extraPostFilesRules = do
+   route idRoute
+   compile $ do
+      path <- drop 2 <$> getResourceFilePath -- drop leading "./"
+      contents <- unsafeCompiler $ readFile path
+      return $ Item (fromFilePath path) contents
+
+postRules :: Identifier -> Context String -> Rules ()
+postRules templateIdent context = do
+   route $ setExtension "html"
+   compile $ do
+      extrafiles <- getExtraPostFiles
+      pandocCompilerWithTransform defaultHakyllReaderOptions defaultHakyllWriterOptions
+                  (pandocIncludeFilter extrafiles)
+         >>= loadAndApplyTemplate templateIdent context
+         >>= loadAndApplyTemplate "templates/default.html" context
+         >>= relativizeUrls
+
+getExtraPostFiles :: Compiler [(String, String)]
+getExtraPostFiles = do
+   path <- (drop 2 . dropExtension) <$> getResourceFilePath -- drops leading "./" and an extension
+   map (fromItem path) <$> loadAll (fromGlob $ path </> "*")
+ where fromItem :: String -> Item String -> (String, String)
+       -- drop leading directories up to a post name and also drop following "/"
+       fromItem path (Item ident body) = (drop (length path + 1) $ toFilePath ident, body)
+
+-- From http://johnmacfarlane.net/pandoc/scripting.html#include-files
+pandocIncludeFilter :: [(String, String)] -> Pandoc -> Pandoc
+pandocIncludeFilter extrafiles = walk go
+ where go :: Block -> Block
+       go cb@(CodeBlock props@(_, _, namevals) _) =
+          maybe cb (CodeBlock props) $
+             flip lookup extrafiles =<< lookup "include" namevals
+       go b = b
 
 draftsContext :: [Item String] -> Context String
 draftsContext posts =  listField "posts" draftContext (return posts)
